4ac4245815ab30f8a5317757725cf5423996be2f
prasad <psam165@gmail.com>
2016-02-19 15:30:46 -0500

    Greetings with path param

--- src/main/java/jetty/jersey/resource/Greeter.java
+++ src/main/java/jetty/jersey/resource/Greeter.java
@@ -2,16 +2,24 @@ package jetty.jersey.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("/Greet")
 public class Greeter {
 	
 	@GET
-	 @Produces("text/plain")
+	@Produces("text/plain")
 	public String simple(){
 		
 		return "Hello ...World!";
+	}
+	
+	@GET
+	@Produces("text/plain")
+	@Path("/{fname}")
+	public String greetByName(@PathParam(value = "fname") String param){
+		return "Hello .."+param;
 	}
 
 }

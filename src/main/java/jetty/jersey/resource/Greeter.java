ab3ea5060c5ad41594d92bbbb6f8f3e5fb1b3499
prasad <psam165@gmail.com>
2016-12-05 12:47:20 -0500

    Changes for Jetty development

--- src/main/java/jetty/jersey/resource/Greeter.java
+++ src/main/java/jetty/jersey/resource/Greeter.java
@@ -1,9 +1,11 @@
 package jetty.jersey.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Path("/Greet")
 public class Greeter {
@@ -22,4 +24,11 @@ public class Greeter {
 		return "Hello .."+param;
 	}
 
+	@POST
+	public Response insertGreet(String body){
+		System.out.println(body);
+		
+		return Response.ok().build();
+	}
+	
 }
